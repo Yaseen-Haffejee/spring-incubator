a531b14e18ab55bdf3811f200b06efdc88d2b194
Yaseen Haffejee <yaseen.haffejee@entelect.co.za>
2023-03-15 16:34:21 +0200

    Notification service

---
+++ booking-service/src/main/java/entelect/training/spring/booking/service/NotificationQueueService.java
@@ -0,0 +1,2 @@
+package entelect.training.spring.booking.service;public class NotificationQueueService {
+}
---
+++ notification-service/src/main/java/entelect/training/incubator/spring/notification/services/QueueListener.java
@@ -0,0 +1,2 @@
+package entelect.training.incubator.spring.notification.services;public class QueueListeners {
+}
